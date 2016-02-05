e51e757fb959b986cba8b55e83f03659c437d2b8
Tyler Reinecke <tylerreinecke@gmail.com>
2016-02-05 14:01:24 -0800

    Update PromptWindow.java

--- TextDisplay/src/PromptWindow.java
+++ TextDisplay/src/PromptWindow.java
@@ -38,101 +38,101 @@ public class PromptWindow implements ActionListener {
 	}
 
 	public PromptWindow() {
-			//Speed and GUI
-				frame = new JFrame("Set your speed");
-				panel = new JPanel();
-				speedInfo = new JLabel();
-				speedSet = new JTextField(5);
-				paste = new JLabel();
-				textPaste = new JTextArea();
+		//Speed and GUI
+			frame = new JFrame("Set your speed");
+			panel = new JPanel();
+			speedInfo = new JLabel();
+			speedSet = new JTextField(5);
+			paste = new JLabel();
+			textPaste = new JTextArea();
+		
+			frame.setVisible(true);
 			
-				frame.setVisible(true);
-				
-				panel.setLayout(null);	
-				
-				speedInfo.setBounds(10, 10, 350, 30);
-				speedSet.setBounds(360, 10, 80, 30);
-				paste.setBounds(10, 50, 500, 30);
-				textPaste.setBounds(320, 55, 470, 600);
-				
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setLocationRelativeTo(null);
+			panel.setLayout(null);	
 			
-				frame.add(panel);
-				panel.add(speedInfo);
-				panel.add(speedSet);
-				panel.add(paste);
-				panel.add(textPaste);
+			speedInfo.setBounds(10, 10, 350, 30);
+			speedSet.setBounds(360, 10, 80, 30);
+			paste.setBounds(10, 50, 500, 30);
+			textPaste.setBounds(320, 55, 470, 600);
 			
-				speedInfo.setText("Please set the word display speed in words per minute:");
-				paste.setText("Or input desired text here if no file was selected: ");
-				
-				textPaste.setColumns(2);
-				textPaste.setRows(100);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocationRelativeTo(null);
+		
+			frame.add(panel);
+			panel.add(speedInfo);
+			panel.add(speedSet);
+			panel.add(paste);
+			panel.add(textPaste);
+		
+			speedInfo.setText("Please set the word display speed in words per minute:");
+			paste.setText("Or input desired text here if no file was selected: ");
 			
-			//JOptionPane
-				int result = JOptionPane.showConfirmDialog(null, "Do you want to display text from a file?", null, JOptionPane.YES_NO_OPTION);
+			textPaste.setColumns(2);
+			textPaste.setRows(100);
+		
+		//JOptionPane
+			int result = JOptionPane.showConfirmDialog(null, "Do you want to display text from a file?", null, JOptionPane.YES_NO_OPTION);
+		
+		//Run Button
+			run = new JButton("Run");
+			run.setBounds(100, 160, 80, 30);
+			panel.add(run);
+			frame.setSize(800, 800);
 			
-			//Run Button
-				run = new JButton("Run");
-				run.setBounds(100, 160, 80, 30);
-				panel.add(run);
-				frame.setSize(800, 800);
+			run.addActionListener(this);	
+			
+		//TextPaste or File		
+			if(result == JOptionPane.YES_OPTION)
+			{
+				//FileDialog
+					fd = new FileDialog(frame, "Test", FileDialog.LOAD);
+			        fd.setVisible(true);
+			        System.out.println(fd.getFile());
+			        String address = fd.getDirectory() + fd.getFile();
+			        file = new File(address);
+		            
+		        //File Reader
+					String text = FileReader.setFile(file);
+					System.out.println(text);
 				
-				run.addActionListener(this);	
-				
-			//TextPaste or File		
-				if(result == JOptionPane.YES_OPTION)
-				{
-					//FileDialog
-						fd = new FileDialog(frame, "Test", FileDialog.LOAD);
-				        fd.setVisible(true);
-				        System.out.println(fd.getFile());
-				        String address = fd.getDirectory() + fd.getFile();
-				        file = new File(address);
-			            
-			        //File Reader
-						String text = FileReader.setFile(file);
-						System.out.println(text);
+				//Document Analyzer and Splitter for FileReader
+					words = new ArrayList<String>();
+					String distext = "";
+						
+					for (int i = 0; i < text.length(); i++) {
+						if(text.charAt(i) == ' '||i == text.length() - 1) {
+							words.add(distext);
+							distext = "";
+						}
+						else {
+							distext += text.charAt(i);
+						}	
+					}
 					
-					//Document Analyzer and Splitter for FileReader
-						words = new ArrayList<String>();
-						String distext = "";
+					input = true;
+			}
+			else if(result == JOptionPane.NO_OPTION)
+			{
+				//Text Paste
+					String paste = textPaste.getText();
+					
+					String disPaste = "";
+					wordsPaste = new ArrayList<String>();
+					
+					for (int i = 0; i < paste.length(); i++) {
+						if(paste.charAt(i) == ' '||i == paste.length() - 1) {
+							wordsPaste.add(disPaste);
 							
-						for (int i = 0; i < text.length(); i++) {
-							if(text.charAt(i) == ' '||i == text.length() - 1) {
-								words.add(distext);
-								distext = "";
-							}
-							else {
-								distext += text.charAt(i);
-							}	
+							disPaste = "";
 						}
-						
-						input = true;
-				}
-				else if(result == JOptionPane.NO_OPTION)
-				{
-					//Text Paste
-						String paste = textPaste.getText();
-						
-						String disPaste = "";
-						wordsPaste = new ArrayList<String>();
-						
-						for (int i = 0; i < paste.length(); i++) {
-							if(paste.charAt(i) == ' '||i == paste.length() - 1) {
-								wordsPaste.add(disPaste);
-								
-								disPaste = "";
-							}
-							else {
-								disPaste += paste.charAt(i);
-								System.out.println(paste.charAt(i));
-							}	
-						}
-						
-						input = false;
-				}
+						else {
+							disPaste += paste.charAt(i);
+							System.out.println(paste.charAt(i));
+						}	
+					}
+					
+					input = false;
+			}
 		
 	}
 	
